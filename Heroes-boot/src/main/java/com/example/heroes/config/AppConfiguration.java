c082e7d6d40d891bc13829fee4d909da46727443
MariaGallegoGarcia <valdellobin@gmail.com>
2021-06-05 22:22:57 +0200

    [Jira-6] Improve the database initialization

--- Heroes-boot/src/main/java/com/example/heroes/config/AppConfiguration.java
+++ Heroes-boot/src/main/java/com/example/heroes/config/AppConfiguration.java
@@ -1,52 +1,11 @@
 package com.example.heroes.config;
 
-import com.example.heroes.model.entity.HeroeEntity;
-import com.example.heroes.model.repository.HeroesRepository;
-import java.util.stream.Stream;
-import org.springframework.boot.ApplicationRunner;
+import com.example.heroes.model.config.R2DBCConfigurationProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.r2dbc.core.DatabaseClient;
-import reactor.core.publisher.Flux;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication(scanBasePackages = "com.example.heroes")
+@EnableConfigurationProperties(R2DBCConfigurationProperties.class)
 public class AppConfiguration {
-
-  @Bean
-  ApplicationRunner init(HeroesRepository repository, DatabaseClient client) {
-    return args -> {
-      client.sql("create table IF NOT EXISTS Heroe"
-          + "(id SERIAL PRIMARY KEY,"
-          + " text varchar (255) not null,"
-          + " completed boolean"
-          + " default false);")
-          .fetch()
-          .first()
-          .subscribe();
-      client.sql("DELETE FROM Heroe;").fetch().first().subscribe();
-
-      Stream<HeroeEntity> stream = Stream.of(
-          HeroeEntity
-              .builder()
-              .id(1)
-              .name("Aquaman")
-              .build(),
-          HeroeEntity
-              .builder()
-              .id(2)
-              .name("Iron man")
-              .build(),
-          HeroeEntity
-              .builder()
-              .id(3)
-              .name("Súper López")
-              .build());
-
-      // initialize the database
-
-      repository.saveAll(Flux.fromStream(stream)).then().subscribe(); // execute
-
-    };
-  }
 
 }
---
+++ Heroes-model/src/main/java/com/example/heroes/model/config/R2DBCConfigurationProperties.java
@@ -0,0 +1,17 @@
+package com.example.heroes.model.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "r2dbc")
+@Getter
+@Setter
+public class R2DBCConfigurationProperties {
+
+    private String url;
+
+    private String user;
+    private String password;
+
+}
